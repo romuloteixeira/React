cad102479326fb071935e8ddbfa22732abc489e8
Romulo Teixeira <romulo_teixeira@hotmail.com>
2019-09-14 14:19:33 +0100

    Secound delivery

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hi, I`m React App - JSX</h1>
+        <p>This is really working!</p>
       </div>
     );
+
+    // return React.CreateElement('div', {className: 'App'}, null);
   }
 }
 
