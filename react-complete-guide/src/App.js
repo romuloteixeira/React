59745ef1f1fb7f6ee395d630d2475394e4028c02
Romulo Teixeira <romulo_teixeira@hotmail.com>
2019-09-20 22:43:04 +0100

    Using state

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,14 +3,36 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Max', age: 28},
+      {name: 'Wil', age: 46},
+      {name: 'Rose', age: 17}
+    ],
+    otherState: 'some other value'
+  };
+
+  switchNameHandler = () => {
+    // console.log("Was clicked! Passed here.");
+    // DON'T DO THIS:  this.state.persons[0].name = 'Maximilian';
+    this.setState({
+      persons: [
+        {name: 'Maximilian', age: 28},
+        {name: 'Wil', age: 46},
+        {name: 'Rose', age: 18}
+      ]
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I`m React App - JSX</h1>
         <p>This is really working!</p>
-        <Person name="Max" age="20" />
-        <Person name="Wil" age="46">My hobbies: Hource race</Person>
-        <Person name="Rose" age="17"></Person>
+        <button onClick={this.switchNameHandler}>Switch name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[0].age}>My hobbies: Hource race</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[0].age}></Person>
       </div>
     );
 
