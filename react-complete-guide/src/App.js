4e5a167daa9a5d27950324c0891b89fb10780529
Romulo Teixeira <romulo_teixeira@hotmail.com>
2019-09-30 23:10:52 +0100

    Sharing handlers.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -15,14 +15,24 @@ const app = props => {
 
   // console.log(personsState, otherState);
 
-  const switchNameHandler = () => {
+  const switchNameHandler = (newName) => {
     // console.log("Was clicked! Passed here.");
     // DON'T DO THIS:  this.state.persons[0].name = 'Maximilian';
     setPersonsState({
       persons: [
-        {name: 'Maximilian', age: 28},
+        {name: newName, age: 28},
         {name: 'Wil', age: 46},
         {name: 'Rose', age: 18}
+      ]
+    });
+  };
+
+  const nameChangedHandler = (event) => {
+    setPersonsState({
+      persons: [
+        {name: 'Max', age: 28},
+        {name: event.target.value, age: 46},
+        {name: 'Rose', age: 17}
       ]
     });
   };
@@ -31,10 +41,19 @@ const app = props => {
     <div className="App">
       <h1>Hi, I`m React App - JSX</h1>
       <p>This is really working!</p>
-      <button onClick={switchNameHandler}>Switch name</button>
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
-      <Person name={personsState.persons[1].name} age={personsState.persons[0].age}>My hobbies: Hource race</Person>
-      <Person name={personsState.persons[2].name} age={personsState.persons[0].age}></Person>
+      <button onClick={() => switchNameHandler('Maximilian!!')}>Switch name</button>
+      <Person 
+        name={personsState.persons[0].name} 
+        age={personsState.persons[0].age} />
+      <Person 
+        name={personsState.persons[1].name} 
+        age={personsState.persons[1].age}
+        click={switchNameHandler.bind(this, 'Max!')}
+        changed={nameChangedHandler}
+        >My hobbies: Hource race</Person>
+      <Person 
+        name={personsState.persons[2].name} 
+        age={personsState.persons[2].age}></Person>
     </div>
   );
   // return React.CreateElement('div', {className: 'App'}, null);
